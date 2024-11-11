e6e0b4e3361f99dfb599712335b217e8292fc550
Robinpro10 <robinalzalara@gmail.com>
2024-11-10 23:08:40 -0500

    Creacion de Proyecto

---
+++ AronPanAplicacion/AronPanAplicacion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AronPanAplicacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
